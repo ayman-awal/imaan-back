8991e03cc85b5e2793703b360c0f926975053199
ayman-awal <ayman.awal36@gmail.com>
2025-02-27 12:42:49 +0600

    setup Express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
