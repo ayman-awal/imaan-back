586f2a1f39aea6131b3a2b3015889a3a4ca6aa29
ayman-awal <ayman.awal36@gmail.com>
2025-03-04 14:05:15 +0600

    add user Registration API

---
+++ prisma/migrations/20250304063159_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "user_type" SET DEFAULT 'user';
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const userRoutes = require('./src/routers/users');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use("/api/users", userRoutes);
 
 
 const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
